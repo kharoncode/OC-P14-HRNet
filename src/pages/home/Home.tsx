b1c74e661e23651c8c82b2648e577a3e8f658969
Collin Rémi <sharon.code@proton.me>
2024-01-13 20:30:11 +0100

    Init P14

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+import styles from './home.module.css'
+
+const Home = () => {
+  return (
+    <div className={styles.container}>Home</div>
+  )
+}
+
+export default Home
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import * as path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+   plugins: [react()],
+   resolve: {
+      alias: [
+         { find: '@', replacement: path.resolve(__dirname, 'src') },
+         {
+            find: '@router',
+            replacement: path.resolve(__dirname, 'src/router'),
+         },
+         {
+            find: '@assets',
+            replacement: path.resolve(__dirname, 'src/assets'),
+         },
+         {
+            find: '@components',
+            replacement: path.resolve(__dirname, 'src/components'),
+         },
+         { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+         { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
+      ],
+   },
+});
