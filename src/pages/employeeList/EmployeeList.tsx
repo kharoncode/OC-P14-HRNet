5608a479791e0d9142ffafced8ee702ac2b4ac37
Collin Rémi <sharon.code@proton.me>
2024-01-15 16:07:45 +0100

    edit page content

--- src/pages/employeeList/EmployeeList.tsx
+++ src/pages/employeeList/EmployeeList.tsx
@@ -1,5 +1,11 @@
+import styles from './employeeList.module.css';
+
 const EmployeeList = () => {
-   return <div>EmployeeList</div>;
+   return (
+      <div className={styles.container}>
+         <h2>Current Employees</h2>
+      </div>
+   );
 };
 
 export default EmployeeList;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -45,8 +45,6 @@ const Home = () => {
 
    return (
       <div className={styles.container}>
-         <h1>HRnet</h1>
-         <Link to="employee-list">View Current Employees</Link>
          <h2>Create Employee</h2>
          <form
             className={styles.formContainer}
