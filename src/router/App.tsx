b05dddf4ced01aa0d90c4f187e356a5f9140ec57
Collin Rémi <sharon.code@proton.me>
2024-01-16 11:58:42 +0100

    add persistor

--- src/router/App.tsx
+++ src/router/App.tsx
@@ -3,21 +3,27 @@ import EmployeeList from '@/pages/employeeList/EmployeeList';
 import NavBar from '@/components/navBar/NavBar';
 import CreateEmployee from '@/pages/createEmployee/CreateEmployee';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { persistor, store } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
    return (
       <Provider store={store}>
-         <Router>
-            <NavBar>
-               <main>
-                  <Routes>
-                     <Route path="/" element={<CreateEmployee />} />
-                     <Route path="/employee-list" element={<EmployeeList />} />
-                  </Routes>
-               </main>
-            </NavBar>
-         </Router>
+         <PersistGate loading={null} persistor={persistor}>
+            <Router>
+               <NavBar>
+                  <main>
+                     <Routes>
+                        <Route path="/" element={<CreateEmployee />} />
+                        <Route
+                           path="/employee-list"
+                           element={<EmployeeList />}
+                        />
+                     </Routes>
+                  </main>
+               </NavBar>
+            </Router>
+         </PersistGate>
       </Provider>
    );
 }
--- src/router/store.tsx
+++ src/router/store.tsx
@@ -2,16 +2,29 @@ import { createEmployeeSlice } from '@/pages/createEmployee/createEmployeeSlice'
 import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { combineReducers } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+   key: 'root',
+   storage,
+};
 
 const reducers = combineReducers({
    createEmployee: createEmployeeSlice.reducer,
 });
 
-export const store = configureStore({
-   reducer: reducers,
+const store = configureStore({
+   reducer: persistReducer(persistConfig, reducers),
+   middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export type RootState = ReturnType<typeof reducers>;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+const persistor = persistStore(store, null);
+
+export { store, persistor };
