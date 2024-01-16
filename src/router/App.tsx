fa1af5d926e7261474498df8f9f8086dd12b0638
Collin Rémi <sharon.code@proton.me>
2024-01-16 11:27:16 +0100

    add store

--- src/router/App.tsx
+++ src/router/App.tsx
@@ -2,19 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EmployeeList from '@/pages/employeeList/EmployeeList';
 import NavBar from '@/components/navBar/NavBar';
 import CreateEmployee from '@/pages/createEmployee/CreateEmployee';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 function App() {
    return (
-      <Router>
-         <NavBar>
-            <main>
-               <Routes>
-                  <Route path="/" element={<CreateEmployee />} />
-                  <Route path="/employee-list" element={<EmployeeList />} />
-               </Routes>
-            </main>
-         </NavBar>
-      </Router>
+      <Provider store={store}>
+         <Router>
+            <NavBar>
+               <main>
+                  <Routes>
+                     <Route path="/" element={<CreateEmployee />} />
+                     <Route path="/employee-list" element={<EmployeeList />} />
+                  </Routes>
+               </main>
+            </NavBar>
+         </Router>
+      </Provider>
    );
 }
 
---
+++ src/router/store.tsx
@@ -0,0 +1,17 @@
+import { createEmployeeSlice } from '@/pages/createEmployee/createEmployeeSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { combineReducers } from 'redux';
+
+const reducers = combineReducers({
+   createEmployee: createEmployeeSlice.reducer,
+});
+
+export const store = configureStore({
+   reducer: reducers,
+});
+
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export type RootState = ReturnType<typeof reducers>;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
