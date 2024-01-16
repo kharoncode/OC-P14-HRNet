0155752c63b63d6f55f52cebec5113cf6ba6d788
Collin Rémi <sharon.code@proton.me>
2024-01-16 21:12:20 +0100

    add hrnet-database packages

--- src/pages/employeeList/EmployeeList.tsx
+++ src/pages/employeeList/EmployeeList.tsx
@@ -1,6 +1,8 @@
 import { useSelector } from 'react-redux';
 import styles from './employeeList.module.css';
 import { getEmployeeList } from '@/router/selectors';
+//import { DataBase } from 'hrnet-database';
+import { DataBase } from '../../../../../packages/oc-p14-dataBase/src/dataBase';
 
 const EmployeeList = () => {
    const employeeList = useSelector(getEmployeeList);
@@ -8,7 +10,9 @@ const EmployeeList = () => {
    return (
       <div className={styles.container}>
          <h1>Current Employees</h1>
-         <div className={styles.tabContainer}></div>
+         <div className={styles.tabContainer}>
+            <DataBase name={'bob'} />
+         </div>
       </div>
    );
 };
